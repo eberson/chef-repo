b2d8daa254a125af18490c718b00953dca7abc31
Eberson Oliveira <eberson.oliveira@hpe.com>
2016-03-09 15:32:00 -0300

    criacao do VagrantFile

---
+++ .chef/knife.rb
@@ -0,0 +1,14 @@
+current_dir = File.dirname(__FILE__)
+chelog_level             :debug
+log_location             STDOUT
+node_name                "eberson"
+client_key               "#{current_dir}/eberson.pem"
+validation_client_name   "sde-validator"
+validation_key           "#{current_dir}/sde-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/sde"
+cache_type               "BasicFile"
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright "SDE"
+cookbook_license "apachev2"
+cookbook_email "eberson.oliveira@gmail.com"
